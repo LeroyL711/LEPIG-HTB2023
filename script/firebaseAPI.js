6080cd88f391c257db08fb418b52a9bbb1355d56
ECHOOWWW <ywang623@my.bcit.com>
2023-03-11 10:12:19 -0800

    Added firebaseAPI.js and attached to the template

---
+++ script/firebaseAPI.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDGGiQ7COuRPQq_9lVGQQsI9nYSo_IRWI0",
+  authDomain: "planit-17b71.firebaseapp.com",
+  projectId: "planit-17b71",
+  storageBucket: "planit-17b71.appspot.com",
+  messagingSenderId: "566872374929",
+  appId: "1:566872374929:web:862ac12dcfc9a251edc930"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = firebase.firestore();
