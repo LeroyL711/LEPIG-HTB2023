84dcf588ba5959830e684793ab270c18d9beb260
Gin Lu <glu23@my.bcit.ca>
2023-03-11 12:39:00 -0800

    Added in more stuff for index

--- public/js/firebaseAPI.js
+++ public/js/firebaseAPI.js
@@ -8,5 +8,5 @@ var firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
