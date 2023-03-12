36562d9a3d263244c2d7b3034ccdd9e63b389ed6
ECHOOWWW <ywang623@my.bcit.com>
2023-03-12 12:34:03 -0700

    got the detail page working

--- public/js/detail.js
+++ public/js/detail.js
@@ -1,53 +1,85 @@
-// get the document ID from localStorage
-const docId = localStorage.getItem('docId');
-console.log(docId);
+const urlParams = new URLSearchParams(window.location.search);
+const docId = urlParams.get('docID');
+let applicationRef;
 // get a reference to the "users" collection
-const usersRef = db.collection("users");
+firebase.firestore().collection("users").get()
+  .then(querySnapshot => {
+    querySnapshot.forEach(userDoc => {
+      firebase.firestore().collection("users").doc(userDoc.id).collection("applications").doc(docId).get()
+        .then(applicationDoc => {
+          if (applicationDoc.exists) {
+            // get the user ID
+            const userId = userDoc.id;
+            console.log(`User ID: ${userId}`);
+            // get a reference to the application document
+            applicationRef = db.collection("users").doc(userId).collection("applications").doc(docId);
 
-// perform a query to search for the document that contains the application ID
-usersRef.where(`applications.${docId}`, '!=', null).get()
-  .then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      // get the user ID
-      const userId = doc.id;
-      console.log(`User ID: ${userId}`);
-      // get a reference to the application document
-      const applicationRef = doc.ref.collection("applications").doc(docId);
-      // Retrieve the existing data from the document
-      applicationRef.get().then((doc) => {
-        if (doc.exists) {
-          const existingData = doc.data();
-          // Use the existing data to populate the form
-          document.getElementById("company-editview").value = existingData.company;
-          document.getElementById("job-title-editview").value = existingData.title;
-          document.getElementById("link-editview").value = existingData.link;
-          document.getElementById("status-editview"); // get the dropdown element by ID
-          let selectedStatusIndex = statusDropdown.selectedIndex; // get the index of the selected option
-          statusDropdown.options[selectedStatusIndex].value = existingData.status; // get the value of the selected option
-          document.getElementById("job-location-editview").value = existingData.location;
-          document.getElementById("date-applied-editview").value = existingData.applied_date;
-          document.getElementById("date-updated-editview").value = existingData.updated_date;;
-          document.getElementById("note-editview").value = existingData.note;;
-          document.getElementById("job-description-editview").value = existingData.job_description;;
-          document.getElementById("contact-email-editview").value = existingData.contact_email;;
-          document.getElementById("contact-number-editview").value = existingData.contact_number;;
+            // Retrieve the existing data from the document
+            applicationRef.get().then((doc) => {
+              if (doc.exists) {
+                const existingData = doc.data();
+                console.log("Existing data:", doc.data());
 
-        } else {
-          console.log("No such document!");
-        }
-      }).catch((error) => {
-        console.log("Error getting document:", error);
-      });
-    });
-  })
-  .catch((error) => {
-    console.log("Error getting documents: ", error);
+                // Use the existing data to populate the form
+                document.getElementById("company-editview").value = existingData.company;
+                document.getElementById("job-title-editview").value = existingData.title;
+                document.getElementById("link-editview").value = existingData.link;
+                // get the dropdown element by ID
+                const statusDropdown = document.getElementById("status-editview");
+                let selectedStatusIndex = statusDropdown.selectedIndex; // get the index of the selected option
+                statusDropdown.options[selectedStatusIndex].value = existingData.status; // get the value of the selected option
+                document.getElementById("job-location-editview").value = existingData.location;
+                document.getElementById("date-applied-editview").value = existingData.applied_date;
+                document.getElementById("date-updated-editview").value = existingData.updated_date;
+                document.getElementById("note-editview").value = existingData.note;
+                document.getElementById("job-description-editview").value = existingData.job_description;
+                document.getElementById("contact-email-editview").value = existingData.contact_email;
+                document.getElementById("contact-number-editview").value = existingData.contact_number;
+              } else {
+                console.log("No such document!");
+              }
+            })
+              .catch(error => {
+                console.log("Error getting application document:", error);
+              });
+          };
+        })
+        .catch(error => {
+          console.log("Error getting users:", error);
+        });
+    })
+
   });
 
 // add click event listener to edit button
 const updateBtn = document.querySelector('#update-btn-editview');
+
 updateBtn.addEventListener('click', () => {
+  let Company = document.getElementById("company-editview").value;
+  let Title = document.getElementById("job-title-editview").value;
+  let Link = document.getElementById("link-editview").value;
+  let statusDropdown = document.getElementById("status-editview"); // get the dropdown element by ID
+  let selectedStatusIndex = statusDropdown.selectedIndex; // get the index of the selected option
+  let Status = statusDropdown.options[selectedStatusIndex].value; // get the value of the selected option
+  let Location = document.getElementById("job-location-editview").value;
+  let Applied = document.getElementById("date-applied-editview").value;
+  let Updated = document.getElementById("date-updated-editview").value;;
+  let Note = document.getElementById("note-editview").value;;
+  let Description = document.getElementById("job-description-editview").value;;
+  let Email = document.getElementById("contact-email-editview").value;;
+  let Number = document.getElementById("contact-number-editview").value;;
   const newData = {
+    company: Company,
+    title: Title,
+    link: Link,
+    status: Status,
+    location: Location,
+    applied_date: Applied,
+    updated_date: Updated,
+    note: Note,
+    job_description: Description,
+    contact_email: Email,
+    contact_number: Number
   };
   applicationRef.update(newData)
     .then(() => {
@@ -57,6 +89,5 @@ updateBtn.addEventListener('click', () => {
       console.error('Error updating document: ', error);
     });
 });
-
 
 
