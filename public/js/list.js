fc8225b25e9606068f6fd8f2ea4c8c4bd21ab93e
Gin Lu <glu23@my.bcit.ca>
2023-03-11 23:32:34 -0800

    Updated some functions

--- public/js/list.js
+++ public/js/list.js
@@ -1,30 +1,60 @@
-
-$(document).ready(function () {
-  $("#myInput").on("keyup", function () {
-    var value = $(this).val().toLowerCase();
-    $(".dropdown-menu li").filter(function () {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-});
-
 function addApplicationItem(name, company, position, status, id) {
   const applicationItem = document.createElement('li');
   applicationItem.innerHTML = `
-    <div class="application-name">${name}</div>
-    <div class="application-company">${company}</div>
-    <div class="application-position">${position}</div>
+    <div class="application-name">${company}</div>
+    <div class="application-company">${title}</div>
+    <div class="application-position">${location}</div>
     <div class="application-status">${status}</div>
   `;
 
   // Add a unique identifier attribute to the HTML element
   applicationItem.setAttribute('data-id', id);
 
-  applicationList.appendChild(applicationItem);
+  const tabId = getTabIdFromStatus(status);
+  const tabContent = document.getElementById(tabId);
+  const list = tabContent.querySelector('ul');
+  list.appendChild(applicationItem);
 }
 
 
-// Load the user's applications from Firestore
+const applicationsRef = firebase.firestore().collection('applications');
+
+applicationsRef.get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
+    console.log('Data:', data);
+    
+    const listItem = document.createElement('li');
+    listItem.textContent = `${data.Title} at ${data.Company}`;
+    
+    const status = data.Status;
+    console.log('Status:', status);
+    
+    const tabId = getTabIdFromStatus(status);
+    console.log('Tab Id:', tabId);
+    
+    const tabContent = document.getElementById(tabId);
+    console.log('Tab Content:', tabContent);
+    
+    const list = tabContent.querySelector('ul');
+    console.log('List:', list);
+    
+    list.appendChild(listItem);
+  });
+});
+
+function getTabIdFromStatus(status) {
+  switch (status) {
+    case 'Preparing': return 'tab2';
+    case 'Submitted': return 'tab3';
+    case 'Shortlisted': return 'tab4';
+    case 'Negotiating': return 'tab5';
+    case 'Offered': return 'tab6';
+    case 'Declined': return 'tab7';
+    default: return 'tab1'; 
+  }
+}
+
 function loadApplicationsFromFirestore() {
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
@@ -41,76 +71,18 @@ function loadApplicationsFromFirestore() {
 }
 
 function openTab(event, tabId) {
-  // Declare variables
-  var i, tabcontent, tablinks;
+  const tabcontents = document.querySelectorAll('.tabcontent');
+  tabcontents.forEach(tabcontent => {
+    tabcontent.style.display = 'none';
+  });
 
-  // Get all tab content elements and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+  const tablinks = document.querySelectorAll('.tablinks');
+  tablinks.forEach(tablink => {
+    tablink.classList.remove('active');
+  });
 
-  // Get all tab links and remove the active class
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+  const tabcontent = document.querySelector(`#${tabId}`);
+  tabcontent.style.display = 'block';
 
-  // Show the selected tab content and mark the corresponding tab link as active
-  document.getElementById(tabId).style.display = "block";
-  event.currentTarget.className += " active";
-
-  // Retrieve documents based on the selected tab's id
-  if (tabId === "tab1") {
-    // All documents
-    db.collection("applications").get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        const { name, company, position, status } = doc.data();
-        addApplicationItem(name, company, position, status, doc.id);
-      });
-    }).catch((error) => {
-      console.error("Error loading applications from Firestore: ", error);
-    });
-  } else {
-    // Documents with matching status
-    const status = document.getElementById(tabId).getAttribute("data-tab-id");
-    db.collection("applications").where("status", "==", status).get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        const { name, company, position, status } = doc.data();
-        addApplicationItem(name, company, position, status, doc.id);
-      });
-    }).catch((error) => {
-      console.error("Error loading applications from Firestore: ", error);
-    });
-  }
+  event.currentTarget.classList.add('active');
 }
-
-// function populateApplications() {
-//   let listTemplate = document.getElementById("applicationTemplate");
-
-//   let params = new URL(window.location.href) //get the url from the searbar
-//   let listID = params.searchParams.get("docID")
-  
-//   // doublecheck: is your collection called "Reviews" or "reviews"?
-//   db.collection("applications").where( "usersD", "==", listID).get()
-//       .then(allApplications => {
-//           applications=allApplications.docs;
-//           console.log(applications);
-//           applications.forEach(doc => {
-//               var company = doc.data().company; //gets the name field
-
-//               let reviewCard = hikeCardTemplate.content.cloneNode(true);
-//               reviewCard.querySelector('.title').innerHTML = title;     //equiv getElementByClassName
-//               reviewCard.querySelector('.time').innerHTML = new Date(time).toLocaleString();    //equiv getElementByClassName
-//               reviewCard.querySelector('.level').innerHTML = `level: ${level}`;
-//               reviewCard.querySelector('.season').innerHTML = `season: ${season}`;
-//               reviewCard.querySelector('.scrambled').innerHTML = `scrambled: ${scrambled}`;  //equiv getElementByClassName
-//               reviewCard.querySelector('.flooded').innerHTML = `flooded: ${flooded}`;  //equiv getElementByClassName
-//               reviewCard.querySelector('.description').innerHTML = `Description: ${description}`;
-//               hikeCardGroup.appendChild(reviewCard);
-//           })
-//       })
-// }
-// function populateApplications();
-
-
