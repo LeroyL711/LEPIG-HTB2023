12b4e179a1fe130beb6a1ba9eb70f3b1e38fa345
parinrava <pravanbakhsh@my.bcit.ca>
2023-03-11 21:02:11 -0800

    added about-us.html in to server.js

--- server.js
+++ server.js
@@ -66,6 +66,10 @@ app.get('/footer.html', (req, res) => {
   res.sendFile(__dirname + '/text/footer.html');
 });
 
+app.get('/footer.html', (req, res) => {
+  res.sendFile(__dirname + '/text/about-us.html');
+});
+
 // For resource not found (i.e., 404)
 app.use(function (req, res, next) {
   res.status(404).send("<html><head><title>Page not found!</title></head><body><p>Nothing here.</p></body></html>");
